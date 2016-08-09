3598853b4f22dacc27de2cedaf20e10a90cac5cd
Estudiante <Estudiante@FI454402-21.unal.loc>
2016-08-09 15:40:04 -0500

    clase 2

--- src/Ejemplo.java
+++ src/Ejemplo.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  * 
@@ -12,46 +8,30 @@
  * Ejercicio practico de JavaDoc
  */
 public class Ejemplo {
-    /**
-     * Metodo principal de la clase
-     * @param String [] Representa los parametros
-     */
-    public static void main(String []arg){
-	System.out.println("Hola Mundo");
-    }
-    /**
-     * 
-     * @param a Primer parametro
-     * @param b Segundo Parametro
-     * @return 
-     */
-    public int prueba (int a, int b) {
+      public static void main (String [] args) {
       
+    int x = 3;
+    while (x > 0) {
        
-     class Shufflel { 
-      public statics void main (String [] args) {
-       
-    
-    int x=3;
-    while (x>0) {
-        x = x - 1 ; 
-        System.out.print("-");
-     
-    if (x == 1)
+     if (x > 2) {
+    System.out.println("a");
+     }
+     if (x == 2)
+    {
+    System.out.println("b c");
+    }
+     if (x == 1)
     {
     System.out.println("d");
     x = x - 1 ;
     }
-    if (x == 2)
-    {
-    System.out.println("b c");
-    }
-    if (x > 2) {
-    System.out.println("a");
-    }
+   
+    x = x - 1 ; 
+     System.out.print("-");
      }
     }
-
-    return 0;
     }
-}    
+
+     
+    
+   
